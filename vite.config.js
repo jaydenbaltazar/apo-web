36ac7710fa28c76aa5afd699c9d544baf07c89a8
jaydenbaltazar <baltazarjayden25@gmail.com>
2025-11-09 00:45:19 -0800

    Bug Fix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ensures correct asset paths on custom domain
-  build: {
-    outDir: 'dist', // where Vercel will look for built files
-  }
+  base: '/',
 })
